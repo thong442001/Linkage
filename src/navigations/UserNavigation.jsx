ac6545a2010474dfecdcd68675f1e31afe75bafa
thongdeptraiwa <thong442001@gmail.com>
2025-02-14 16:33:36 +0700

    14/2-ThongCheckotCanh

--- src/navigations/UserNavigation.jsx
+++ src/navigations/UserNavigation.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const UserStack = createNativeStackNavigator();
-import Screen1 from '../screens/Register/Screen1';
-import Screen2 from '../screens/Register/Screen2';
-import Screen3 from '../screens/Register/Screen3';
+import Screen1 from '../screens/register/Screen1';
+import Screen2 from '../screens/register/Screen2';
+import Screen3 from '../screens/register/Screen3';
 import Login from '../screens/login/Login';
-import CreatePasswordScreen from '../screens/Register/CreatePasswordScreen';
+import CreatePasswordScreen from '../screens/register/CreatePasswordScreen';
 import Comment from '../components/comment/Comment';
 import FindWithEmail from '../screens/forgot_password/FindWithEmail';
 import FindWithPhone from '../screens/forgot_password/FindWithPhone';
 import CheckEmail from '../screens/forgot_password/CheckEmail';
 import CreateNewPassWord from '../screens/forgot_password/CreateNewPassWord';
 const UserNavigation = () => {
-  return (
-    <UserStack.Navigator
-      initialRouteName="Login"
-      screenOptions={{headerShown: false}}>
-      {/* <UserStack.Screen name="Home" component={Home} />
+    return (
+        <UserStack.Navigator
+            initialRouteName="Login"
+            screenOptions={{ headerShown: false }}>
+            {/* <UserStack.Screen name="Home" component={Home} />
             <UserStack.Screen name="Profile" component={Profile} /> */}
-      {/* <UserStack.Screen name="PostDetail" component={PostDetail} /> */}
-      <UserStack.Screen name="Login" component={Login} />
-      <UserStack.Screen name="Screen1" component={Screen1} />
-      <UserStack.Screen name="Screen2" component={Screen2} />
-      <UserStack.Screen name="Screen3" component={Screen3} />
-      <UserStack.Screen
-        name="CreatePasswordScreen"
-        component={CreatePasswordScreen}
-      />
-      <UserStack.Screen name="FindWithEmail" component={FindWithEmail} />
-      <UserStack.Screen name="FindWithPhone" component={FindWithPhone} />
-      <UserStack.Screen name="CheckEmail" component={CheckEmail} />
-      <UserStack.Screen
-        name="CreateNewPassWord"
-        component={CreateNewPassWord}
-      />
-    </UserStack.Navigator>
-  );
+            {/* <UserStack.Screen name="PostDetail" component={PostDetail} /> */}
+            <UserStack.Screen name="Login" component={Login} />
+            <UserStack.Screen name="Screen1" component={Screen1} />
+            <UserStack.Screen name="Screen2" component={Screen2} />
+            <UserStack.Screen name="Screen3" component={Screen3} />
+            <UserStack.Screen
+                name="CreatePasswordScreen"
+                component={CreatePasswordScreen}
+            />
+            <UserStack.Screen name="FindWithEmail" component={FindWithEmail} />
+            <UserStack.Screen name="FindWithPhone" component={FindWithPhone} />
+            <UserStack.Screen name="CheckEmail" component={CheckEmail} />
+            <UserStack.Screen
+                name="CreateNewPassWord"
+                component={CreateNewPassWord}
+            />
+        </UserStack.Navigator>
+    );
 };
 
 export default UserNavigation;
